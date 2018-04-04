00de8185428c10dd168d8124d591d075ce2cffc0
zhaoliuzeng <1259704800@qq.com>
2018-04-04 15:58:21 +0800

    Initial commit

---
+++ src/main/java/eurekaclient/demo/DemoApplication.java
@@ -0,0 +1,36 @@
+package eurekaclient.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/testinfo")
+	public String serviceInstancesByApplicationName() {
+		return "testinfo";
+	}
+}
